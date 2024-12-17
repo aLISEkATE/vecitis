877e6fe44111cdb21024b1a37bd1811cf4fc2ff2
ipa23.k.strazdina <ipa23.k.strazdina@pjvt.local>
2024-12-17 09:40:49 +0200

    managed to get it into squares

--- functions.php
+++ functions.php
@@ -3,9 +3,13 @@
 //Dump and Die
 function dd($data){
     foreach ($data as $x) {
-        $letter = $x['letter_text'] ? $x['letter_text'] : 'No letter assigned.';
-        echo "<li>" . $x['firstname'] . " " . $x['middlename'] . " " . $x['surname'] . ", " . $x['age'] . " gadi. ";
-        echo "Letter: " . $letter . "</li>";
+         
+    $letter = $x['letter_text'] ? $x['letter_text'] : 'No letter assigned.';
+    echo '<div style="border: 1px solid black; padding: 10px; margin-bottom: 10px; width: 200px;">';
+    echo '<p><strong>Name:</strong> ' . $x['firstname'] . " " . $x['surname'] . '</p>';
+    echo '<p><strong>Age:</strong> ' . $x['age'] . ' gadi</p>';
+    echo '<p><strong>Letter:</strong> ' . $letter . '</p>';
+    echo '</div>';
     }
     die();
     }
